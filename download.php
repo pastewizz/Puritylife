99315378e9d8c43d99411a9076f83a710eb2b5fc
pastewizz <kbofficial555@gmail.com>
2024-06-02 21:03:54 +0700

    Create download.php

---
+++ download.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download Page</title>
+</head>
+<body>
+    <h1>Thank you for your purchase!</h1>
+    <p>Your download link:</p>
+    <a href="book.pdf">Download Book</a> <!-- Replace 'book.pdf' with the actual filename of your book -->
+</body>
+</html>
